258e0db2eed10bcf10b6bd13595e14f2544f471e
Sean McGroarty <mcgroas@tcd.ie>
2017-11-28 21:30:25 +0000

    added: data models

---
+++ Directory-Server/src/Models.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module Models(
+File(..),FileInfo(..),
+doMigrations,runDB
+)where
+
+import Control.Monad.Reader (MonadIO, MonadReader, asks, liftIO)
+import Data.Aeson           (FromJSON, ToJSON)
+import Database.Persist.Sql (SqlPersistT, runMigration, runSqlPool)
+import Database.Persist.TH  (mkMigrate, mkPersist, persistLowerCase,
+                                       share, sqlSettings)
+import GHC.Generics         (Generic)
+import Config               
+import Data.Text            (Text)
+
+data File = File {
+  fileName :: String,
+  filePath :: String,
+  fileContents :: String
+}deriving (Generic, Show)
+instance FromJSON File
+instance ToJSON File
+
+
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+FileInfo json
+    file_name String
+    file_path String
+    nodes [Int]
+    UniqueFile_path file_path
+|]
+
+doMigrations :: SqlPersistT IO ()
+doMigrations = runMigration migrateAll
+
+runDB :: (MonadReader Config m, MonadIO m) => SqlPersistT IO b -> m b
+runDB query = do
+  p <- asks pool
+  liftIO $ runSqlPool query p
