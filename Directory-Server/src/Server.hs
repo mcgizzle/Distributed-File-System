6f79f056ec63f8b1b6dd5604efcb98db8a1d9f59
Sean McGroarty <mcgroas@tcd.ie>
2017-11-28 23:02:04 +0000

    removed: unused imports

--- Directory-Server/src/Server.hs
+++ Directory-Server/src/Server.hs
@@ -17,33 +17,17 @@ import Prelude ()
 import Prelude.Compat
 
 import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson.Compat
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
-import Data.Maybe (fromMaybe)
-import GHC.Generics
-import Network.HTTP.Media ((//), (/:))
 import Network.Wai
 import Network.Wai.Handler.Warp hiding (FileInfo)
 import Servant
-import Servant.Server 
-import System.Directory
-import qualified Data.Aeson.Parser
 import Control.Category     ((<<<), (>>>))
-
-import Control.Monad.IO.Class  (liftIO)
-import Control.Monad.Logger    (runStderrLoggingT)
-import Database.Persist
-import Database.Persist.Postgresql
-import Database.Persist.TH
 
 import Config
 import Models
 import Controller
 
-type DirectoryAPI = ReqBody '[JSON] File :> Get '[JSON] FileInfo 
+type DirectoryAPI = "ls" :> Get '[JSON] [FileInfo]
+               :<|> ReqBody '[JSON] File :> Get '[JSON] FileInfo 
 
 startApp :: Config -> IO ()
 startApp cfg = run 8080 (directoryApp cfg)
@@ -57,8 +41,8 @@ magicToServer cfg = enter (convertMagic cfg >>> NT Handler) directoryServer
 convertMagic cfg = runReaderTNat cfg <<< NT runTheMagic
 
 directoryServer :: MonadIO m => ServerT DirectoryAPI (MagicT m)
-directoryServer = Controller.putFile
-
+directoryServer = Controller.listFiles
+             :<|> Controller.putFile
 
 api :: Proxy DirectoryAPI
 api = Proxy
