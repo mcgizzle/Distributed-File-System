892f005b442791df962b2b474497570b38f620fa
Sean McGroarty <mcgroas@tcd.ie>
2017-11-28 21:27:54 +0000

    added: gets config, runs migrations and starts app

--- Directory-Server/app/Main.hs
+++ Directory-Server/app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import Database.Persist.Sql (runSqlPool)
+
 import Server
+import Config
+import Models (doMigrations)
 
 main :: IO ()
-main = startApp
+main = do
+  c <- getConfig
+  runSqlPool doMigrations (pool c) 
+  startApp c
