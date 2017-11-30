d40122f49dbdce6eec1c4fd7e6b517a0a03cecf2
Sean McGroarty <mcgroas@tcd.ie>
2017-11-30 19:19:12 +0000

    updated: inserts self into database on start-up

--- File-Server/app/Main.hs
+++ File-Server/app/Main.hs
@@ -1,16 +1,56 @@
+{-# LANGUAGE DeriveGeneric              #-} 
+{-# LANGUAGE EmptyDataDecls             #-} 
+{-# LANGUAGE FlexibleContexts           #-} 
+{-# LANGUAGE FlexibleInstances          #-} 
+{-# LANGUAGE GADTs                      #-} 
+{-# LANGUAGE GeneralizedNewtypeDeriving #-} 
+{-# LANGUAGE MultiParamTypeClasses      #-} 
+{-# LANGUAGE OverloadedStrings          #-} 
+{-# LANGUAGE QuasiQuotes                #-} 
+{-# LANGUAGE RecordWildCards            #-} 
+{-# LANGUAGE TemplateHaskell            #-} 
+{-# LANGUAGE TypeFamilies               #-} 
+
 module Main where
 
 import Server
 import Client
 import System.Environment
 
+import Control.Concurrent (forkIO)
+
+import Data.Aeson           (FromJSON, ToJSON)
+import Database.Persist 
+import Database.Persist.Sql 
+import Database.Persist.TH  
+import Database.Persist.Postgresql
+import           Control.Monad.IO.Class  (liftIO)
+import           Control.Monad.Logger    (runStderrLoggingT)
+                                       
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+FileNode json
+    host String
+    port Int 
+    active Bool
+    deriving Show
+|]
+
+connStr = "host=localhost dbname=fs_dev user=root password=root port=5432"
+
+informNetwork :: Int -> IO ()
+informNetwork port = do
+  runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
+    flip runSqlPersistMPool pool $ do
+            runMigration migrateAll
+            insert $ FileNode "localhost" port True
+            return ()
 main :: IO ()
 main = do
-  cmd <- getArgs
-  case cmd of
-    ["server"] -> do
-      putStrLn "Server is running.."
-      runServer
-    ["client"] -> do
-      putStrLn "Starting client..."
-      runClient
+  [port] <- getArgs
+  putStrLn $ "Server is running on port: " ++ port
+  runServer (read port)
+  forkIO $ informNetwork (read port)
+  runServer (read port)
+
+
+           
