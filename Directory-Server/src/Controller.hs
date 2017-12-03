7d82be413d2f0d00bb973911143c5d513390818c
Sean McGroarty <mcgroas@tcd.ie>
2017-12-03 13:01:31 +0000

    added: function to write file

--- Directory-Server/src/Controller.hs
+++ Directory-Server/src/Controller.hs
@@ -8,13 +8,20 @@
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Controller(
-listFiles,putFile
+listFiles,newFile,writeFile,getFile
 )where
 
+import Prelude hiding (writeFile)
+
 import Database.Persist
+import Database.Persist.Sql
 import Servant
+
+import Data.Maybe (fromJust)
+import Data.Time
 
 import Models as M 
 import Config
@@ -29,26 +36,55 @@ listFiles = do
     []   -> throwError err404
     _    -> return $ Prelude.map entityVal res
   
-getFileLoc :: MonadIO m => String -> MagicT m FileInfo
-getFileLoc path = do
-  res <- runDB $ selectFirst [ M.FileInfoFile_path ==. path ] []
+getFile :: MonadIO m => Maybe FilePath -> Maybe String -> MagicT m FileInfo
+getFile path name = do
+  let path' = fromJust path
+  let name' = fromJust name
+  res <- runDB $ selectFirst [ M.FileInfoFile_path ==. path'
+                             , M.FileInfoFile_name ==. name' ] []
   case res of
-    Nothing     -> throwError err404
-    Just res'   -> return $ entityVal res'
-    
-putFile :: MonadIO m => File -> MagicT m FileInfo
-putFile f = do
-  (servers,keys) <- getServers
-  liftIO $ mapM_ (\s -> query (sendFile f) s) servers
-  let f' = FileInfo (fileName f) (filePath f) keys
+    Just res' -> return $ entityVal res'
+    Nothing -> throwError errFileDoesNotExist
+
+writeFile :: MonadIO m => File -> MagicT m FileInfo
+writeFile f@File{..} = do
+  res <- runDB $ selectFirst [ M.FileInfoFile_path ==. filePath
+                             , M.FileInfoFile_name ==. fileName ] []
+  case res of
+    Just res' -> do
+      let nodeKeys = M.fileInfoNodes $ entityVal res'
+      nodes <- mapM nodeFromKey nodeKeys
+      let fileKey = entityKey res'
+      liftIO $ mapM (\n -> query (updateFile f)  n) nodes
+      time <- liftIO getCurrentTime
+      let f' = FileInfo fileName filePath False time nodeKeys
+      runDB $ replace fileKey f'
+      return f'
+    Nothing   -> throwError errFileDoesNotExist
+
+newFile :: MonadIO m => File -> MagicT m FileInfo
+newFile f = do
+  (nodes,keys) <- getAvailableNodes
+  liftIO $ mapM_ (\n -> query (sendFile f) n) nodes
+  time <- liftIO getCurrentTime
+  let f' = FileInfo (fileName f) (filePath f) False time keys
   res <- runDB $ insertUnique f'
   case res of
     Just _  -> return f'
     Nothing -> throwError errFileExists
 
-getServers :: MonadIO m => MagicT m ([M.FileNode],[Key M.FileNode])
-getServers = do
-  res :: [Entity M.FileNode] <- runDB $ selectList [M.FileNodeActive ==. True] []
-  return $ (Prelude.map entityVal res, Prelude.map entityKey res)
+nodeFromKey :: MonadIO m => M.FileNodeId -> MagicT m M.FileNode
+nodeFromKey key = do
+  res :: Maybe (Entity M.FileNode) <- runDB $ selectFirst [ M.FileNodeId ==. key ] []
+  case res of
+    Just res' -> return $ entityVal res'
+    Nothing   -> throwError errNodeDoesNotExist
 
+getAvailableNodes :: MonadIO m => MagicT m ([M.FileNode],[Key M.FileNode])
+getAvailableNodes = do
+  res :: [Entity M.FileNode] <- runDB $ selectList [M.FileNodeActive ==. True] []
+  return (Prelude.map entityVal res, Prelude.map entityKey res)
+
+errFileDoesNotExist = err404 { errBody = "This file does not exits brah"}
 errFileExists = err400 { errBody = "File with this name already exists"}
+errNodeDoesNotExist = err404 { errBody = "Node with this ID does not exist"}
