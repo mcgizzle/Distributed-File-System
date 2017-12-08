afb93c3fc059c4e2be652340775fbc33fd5daea7
Sean McGroarty <mcgroas@tcd.ie>
2017-12-08 21:50:44 +0000

    added: ls,new and write commands working

--- Client/app/Main.hs
+++ Client/app/Main.hs
@@ -4,10 +4,18 @@ import Directory
 
 main :: IO ()
 main = do
-        putStrLn "Welcome to the distributed file system :o"
-        cmd <- getLine
-        case words cmd of
-          ["ls"] -> do
-                  res <- Directory.listFiles
-                  putStrLn $ "And the server says:\n" ++ res
-
+        putStrLn "******* Welcome to the Distributed-File-System **********"
+        loop
+        where 
+                loop = do
+                        cmd <- getLine
+                        case words cmd of
+                          ["ls"]                 -> Directory.listFiles
+                          ["new",name,path]      -> Directory.newFile name path
+                          ["write",name,path]    -> Directory.writeFile name path
+                          _                      -> putStrLn $ "Wrong command u fool, r u thick or summin?" 
+                                                          ++ "\nCOMMANDS:\n" 
+                                                          ++ "---> ls \n" 
+                                                          ++ "---> new <file name> <file path>\n"
+                                                          ++ "---> write <file name> <file path>\n"
+                        loop
--- Client/src/Directory.hs
+++ Client/src/Directory.hs
@@ -1,24 +1,66 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Directory where
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Servant.API
 import Api.Directory
+import Api.File
 import Servant.Client
+
+import Text.Editor
+
+import Data.ByteString.Char8 (pack)
+--import Control.Monad.Trans.Class
+--import Control.Monad.Trans.Either
 
 directoryServer :: (String,Int) 
 directoryServer = ("localhost",8080)
 
-listFiles :: IO String
+getAbsPath :: FileInfo -> String
+getAbsPath f = (fileInfoFile_path f ++ fileInfoFile_name f)
+
+listFiles :: IO ()
 listFiles = do
-        Directory.query listFiles' directoryServer 
+        res <- Directory.query listFiles' directoryServer
+        case res of
+          Right res' -> do
+                  putStrLn "FILES:"
+                  mapM_ (\ f -> do 
+                          putStrLn $ "Name: " ++ fileInfoFile_name f
+                          putStrLn $ "Path: " ++ getAbsPath f ++ "\n") res'
+          Left err   -> putStrLn $ "There was an error: " ++ show err
+
+newFile :: String -> String -> IO ()
+newFile name path = do
+        contents <- runUserEditorDWIM plainTemplate "HERE IS YOUR NEW TEXT FILE\n"
+        let file = File name path ( wrapStr contents)
+        res <- Directory.query (newFile' file) directoryServer
+        case res of
+          Right res' -> putStrLn "File Saved!\n"
+          Left err   -> putStrLn $ "There was an error: " ++ show err
+
+writeFile :: String -> String -> IO ()
+writeFile name path = do
+        res <- Directory.query (getFileLoc' (Just path) (Just name)) directoryServer
+        case res of
+          Right res' -> do
+                  let node = (fileNodeHost $ head res', fileNodePort $ head res')
+                  res'' <- Directory.query (getFile' (Just $ path ++ name)) node 
+                  case res'' of
+                    Right f -> do
+                            contents <- runUserEditorDWIM plainTemplate (pack $ fileContents f)
+                            let file' = File name path (wrapStr contents)
+                            res''' <- Directory.query (writeFile' file') directoryServer
+                            case res''' of 
+                              Right _  -> putStrLn "Success writing to file!\n"
+                              Left err -> putStrLn $ "Error: " ++ show err
+                    Left err -> putStrLn $ "Error: " ++ show err
+          Left err -> putStrLn $ "Error: " ++ show err
 
 --query :: ClientM a -> (String,Int) -> IO String
 query q (host,port) = do
         manager' <- newManager defaultManagerSettings
-        res <- runClientM q (ClientEnv manager' (BaseUrl Http host port ""))
-        case res of
-          Left err  -> return "error"
-          Right res -> return $ show res
+        runClientM q (ClientEnv manager' (BaseUrl Http host port ""))
