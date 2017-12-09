f9df8451e84c01e654b51ba4f186afa48873f847
Sean McGroarty <mcgroas@tcd.ie>
2017-12-09 20:12:41 +0000

    added: timeout on locking

--- Api/src/Api/Locking.hs
+++ Api/src/Api/Locking.hs
@@ -22,13 +22,22 @@ import Data.Aeson           (FromJSON, ToJSON)
 import Database.Persist.Sql (SqlPersistT, runMigration, runSqlPool)
 import Database.Persist.TH  (mkMigrate, mkPersist, persistLowerCase,
                                        share, sqlSettings)
-
+import Servant.API
+import Servant.Client
 import GHC.Generics         (Generic)
+import Data.Time
+
+newtype AccessResponse = AccessResponse{
+  response :: Bool
+}deriving(Generic,Show)
+instance ToJSON AccessResponse
+instance FromJSON AccessResponse
 
 data LockInfo = LockInfo {
   path :: String,
   id :: Int,
-  locked :: Bool
+  locked :: Bool,
+  timeout :: Maybe UTCTime
 }deriving(Generic,Show)
 instance ToJSON LockInfo
 instance FromJSON LockInfo
@@ -37,13 +46,19 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 LockQueue json
     filePath String
     queue [Int]
+    timeout UTCTime 
     UniqueFilePath filePath 
 |]
+lockingApi :: Proxy LockingAPI
+lockingApi = Proxy
 
-
-type LockingAPI = "lock" :> Capture "id" Int :> 
+type LockingAPI = QueryParam "path" FilePath :> Get '[JSON] AccessResponse 
+             :<|> "lock" :> Capture "id" Int :> 
                   QueryParam "path" FilePath :> Get '[JSON] LockInfo
              :<|> "unlock" :>  Capture "id" Int :> 
                   QueryParam "path" FilePath :> Get '[JSON] ()
 
-
+checkLock' :: Maybe FilePath -> ClientM AccessResponse
+lockFile' :: Int -> Maybe FilePath -> ClientM LockInfo
+unlockFile' :: Int -> Maybe FilePath -> ClientM ()
+(checkLock' :<|> lockFile' :<|> unlockFile') = client lockingApi
